a63eb2b69890f860495c2c60ffc4db80cb37e966
zaid-nawaz <zaidnawaz2005@gmail.com>
2026-03-18 20:43:26 +0530

    fixed some errors

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -11,7 +11,7 @@ export const genaiFunction = inngest.createFunction(
         // const question_response = await axios.post("http://localhost:3000/api/mcq",{
         //     video_id : event.data.video_id
         // })
-        const question_response = await axios.post("http://127.0.0.1:8000/generate_mcq",{
+        const question_response = await axios.post("https://acidic-mom-production.up.railway.app/generate_mcq",{
             video_id : event.data.video_id
         })
         
